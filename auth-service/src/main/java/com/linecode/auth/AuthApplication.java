6a751d3b4b5088f2927ddaaa52ec793bd1817255
igor036 <igorjoaquim.pg@gmail.com>
2021-03-09 15:07:12 -0300

    remove unused imports

--- auth-service/src/main/java/com/linecode/auth/AuthApplication.java
+++ auth-service/src/main/java/com/linecode/auth/AuthApplication.java
@@ -1,17 +1,7 @@
 package com.linecode.auth;
 
-import java.util.Arrays;
-import java.util.Collections;
-
-import com.linecode.auth.entity.AuthorityRule;
-import com.linecode.auth.entity.User;
-import com.linecode.auth.repository.AuthorityRuleRepository;
-import com.linecode.auth.repository.UserRepository;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AuthApplication {
