c9a834b0524c7bc49394f12446e17388afda3c44
igor036 <igorjoaquim.pg@gmail.com>
2021-03-10 16:29:05 -0300

    Add eureka client register into payment-service

--- payment-service/src/main/java/com/linecode/payment/PaymentApplication.java
+++ payment-service/src/main/java/com/linecode/payment/PaymentApplication.java
@@ -2,8 +2,10 @@ package com.linecode.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentApplication {
 
 	public static void main(String[] args) {
